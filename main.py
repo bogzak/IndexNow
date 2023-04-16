37ba31a12954507d5af00f0a1ac57909a79ea431
bogzak <bogdan.zaakh@gmail.com>
2023-04-17 00:59:41 +0300

    add searchengine selection

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ def main():
         'Bing': 'www.bing.com'
     }
 
-    # with st.form(key='index_form'):
-    host = st.text_input('Host')
+    with st.form(key='index_form'):
+        host = st.text_input('Host')
 
-    key = st.text_input('Key')
+        key = st.text_input('Key')
 
-    # Get the user's selection from the app
-    select_searchengine = st.selectbox('Select searchengine',['Yandex', 'Bing'], index=False)
-    searchengine = type_mapping[select_searchengine]
+        # Get the user's selection from the app
+        select_searchengine = st.selectbox('Select searchengine',['Yandex', 'Bing'], index=False)
+        searchengine = type_mapping[select_searchengine]
 
-    urls = st.text_area('Enter URLs, one per line')
-    submit_button = st.form_submit_button(label='Index Now')
+        urls = st.text_area('Enter URLs, one per line')
+        submit_button = st.form_submit_button(label='Index Now')
 
     if submit_button:
         indexing = IndexNow(host=host, key=key, searchengine=searchengine)
